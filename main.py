450369edda3650b2837e7532c356798e16e7100f
Billiah Kerubo <93190222+billiahkerubo1@users.noreply.github.com>
2023-10-30 22:21:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,27 @@ from deal import deal_router
 from fastapi import FastAPI
 from app.database import engine
 from app import models
+from fastapi_jwt_auth import AuthJWT
+from app.schema import Settings
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+@AuthJWT.load_config
+def get_config():
+    return Settings 
+
+
+# Define CORS settings
+origins = ["http://localhost", "http://localhost:8080", "http://localhost:8080"]
+# Replace the above origins with the actual domains you want to allow.
+
+customer_router.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -22,6 +42,8 @@ app.include_router(router,)
 app.include_router(customer_router,)
 app.include_router(interact_router,)
 app.include_router(deal_router,)
+
+
 if __name__ == "__main__":
     # Create database tables
     #Base.metadata.create_all(bind=engine)
