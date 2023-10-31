5b95b75faa442f339cc04aaf9115bc2dd6f19787
Billiah Kerubo <93190222+billiahkerubo1@users.noreply.github.com>
2023-10-30 22:18:44 -0700

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,19 +1,31 @@
 # customer.py
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, FastAPI
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models import Customer
+from retrying import retry
+from app.schema import CustomerCreate
+#from fastapi.middleware.throttle import ThrottleLimiter
 
+app = FastAPI()
 customer_router = APIRouter()
 
-# Create a new customer
-@customer_router.post("/v1/customer", tags = ["customers"])
-def create_customer(customer_data: dict, db: Session = Depends(get_db)):
+# Define a custom retry strategy
+@retry(
+    retry_on_exception=lambda exc: isinstance(exc, Exception),  # Retry on any exception
+    wait_fixed=1,  # Wait 1 second between retries
+    stop_max_attempt_number=3,  # Maximum number of retry attempts
+)
+def create_customer_with_retry(
+    customer_data: CustomerCreate,
+    db: Session,
+):
     new_customer = Customer(**customer_data)
     db.add(new_customer)
     db.commit()
     db.refresh(new_customer)
     return new_customer
+
 
 # Retrieve a list of all customers
 @customer_router.get("/v1/customers", tags = ["customers"])
@@ -22,7 +34,7 @@ def get_customers(skip: int = 0, limit: int = 10, db: Session = Depends(get_db))
     return customers
 
 # Retrieve a single customer's details by ID
-@customer_router.get("/v1/customer/{customer_id}", tags = ["customers"])
+@customer_router.get("/v1/customers/{customer_id}", tags = ["customers"])
 def get_customer(customer_id: int, db: Session = Depends(get_db)):
     customer = db.query(Customer).filter(Customer.id == customer_id).first()
     if customer is None:
@@ -30,7 +42,7 @@ def get_customer(customer_id: int, db: Session = Depends(get_db)):
     return customer
 
 # Update customer information
-@customer_router.put("/v1/customer/{customer_id}", tags = ["customers"])
+@customer_router.put("/v1/customers/{customer_id}", tags = ["customers"])
 def update_customer(customer_id: int, customer_data: dict, db: Session = Depends(get_db)):
     existing_customer = db.query(Customer).filter(Customer.id == customer_id).first()
     if existing_customer is None:
@@ -44,7 +56,7 @@ def update_customer(customer_id: int, customer_data: dict, db: Session = Depends
     return existing_customer
 
 # Delete a customer by ID
-@customer_router.delete("/v1/customer/{customer_id}", tags = ["customers"])
+@customer_router.delete("/v1/customers/{customer_id}", tags = ["customers"])
 def delete_customer(customer_id: int, db: Session = Depends(get_db)):
     existing_customer = db.query(Customer).filter(Customer.id == customer_id).first()
     if existing_customer is None:
